1c0c4cff02f411feb6287eb2bac6601d05eabf9b
caballeronegro1 <123416236+caballeronegro1@users.noreply.github.com>
2023-01-23 15:44:27 -0600

    Add files via upload

---
+++ index.blade (2).php
@@ -0,0 +1,13 @@
+@extends('admin.categories.layout')
+
+@section('title','Inicio')
+
+@section('content')
+<div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="https://cdn-icons-png.flaticon.com/512/2345/2345338.png" width="13%">
+    <h1 class="display-5 fw-bold">Bienvenido Administrador</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Explore las opciones para editar, agregar o eliminar productos que necesite.</p>
+    </div>
+  </div>
+@endsection
